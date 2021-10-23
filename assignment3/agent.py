dab42d8ef85302d51c1deceead89e3c2e7391182
Ubuntu <ubuntu@ip-172-31-24-22.us-west-1.compute.internal>
2021-10-23 07:43:53 +0000

    test

--- assignment3/agent.py
+++ assignment3/agent.py
@@ -1,3 +1,17 @@
-#I am kind of thinking of
-print("Hello World")
+import argparse
+import random
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--init', help='called when new game')
+    parser.add_argument('--iterations', help='number of iterations in game')
+    parser.add_argument('--last_opponent_move', help='last opponent move')
+
+    args = parser.parse_args()
+
+    #print(args.last_opponent_move)
+
+    print( random.choice(['confess', 'silent']) )
+    
+
 
